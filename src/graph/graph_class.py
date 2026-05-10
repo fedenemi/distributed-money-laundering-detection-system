f7202561ed08b67cfd2a6af9e193c62ce4950848
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-05-10 14:54:38 -0300

    Graph class created for future needs

---
+++ src/graph/graph_class.py
@@ -0,0 +1,43 @@
+class DirectedGraph:
+    
+    def __init__(self):
+        self.nodes = set()
+        self.adjecencies = {}
+    
+    # Add node to graph
+    def add_node(self, v):
+        self.nodes.add(v)
+        self.adjecencies[v] = set()
+
+    # Delete node from graph    
+    def delete_node(self,v):
+        self.nodes.remove(v)
+        del self.adjecencies[v]
+
+        # Delete connections from other nodes
+        for w in self.nodes:
+            if v in self.adjecencies[w]:
+                del self.adjecencies[w].remove(v)
+
+    # Lazily gets the graph's nodes
+    def get_nodes(self):
+        for v in self.nodes:
+            yield v
+
+    # Delete edge from graph
+    def delete_edge(self, v, w):
+        if self.are_connected(v, w):
+            del self.adjecencies[v][w]
+
+    # Adds edge between nodes
+    def add_edge(self, v, w):
+        self.adjecencies[v].add(w)
+
+    # Returns True if edge v -> w exists and False otherwise
+    def are_connected(self, v, w):
+        return w in self.adjecencies[v]
+
+    # Lazily get a node's neighbours
+    def get_neighbors(self, v):
+        for w in self.adjecencies[v]:
+            yield w
