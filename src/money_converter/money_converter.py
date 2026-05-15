a05f5a59015487869662f6bcccbebf5f5e381372
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-05-14 21:04:51 -0300

    Process function started

--- src/money_converter/money_converter.py
+++ src/money_converter/money_converter.py
@@ -4,11 +4,29 @@ class MoneyConverter(WorkerBase):
 
     def __init__(self):
         super().__init__()
+
+        # Get environment variables
+
         # Currency rates
         self._currency_rates_by_date = {}
 
-    def process(self, data):
+    def _consult_currency_rates_api(self, date, currency):
         pass
-    
+
+    def _get_date_from_datetime(self, datetime_str):
+        pass
+
+    def process(self, data):
+        date = self._get_date_from_datetime(data["Timestamp"])
+
+        # Get currency rates of dates
+        currency_rates = self._currency_rates_by_date.get(date, {})
+
+        # If not currency included
+        currency = data["Payment Currency"]
+        if currency not in currency_rates:
+            currency_rates[currency] = self._consult_currency_rates_api(date, currency)
+
+
     def on_eof(self):
         pass
